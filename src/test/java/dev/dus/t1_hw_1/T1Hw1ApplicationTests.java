5fc7990b9f24ce342b4dc3bb3fa1cfec1784843b
masfuj <gooddayemail@gmail.com>
2024-03-03 17:00:51 +0300

    initial commit

---
+++ src/test/java/dev/dus/t1_hw_1/T1Hw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dus.t1_hw_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1Hw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
